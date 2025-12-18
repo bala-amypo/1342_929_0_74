ff5df9b02512f1d95322582407efe3e31fdd281f
bala-amypo <bala@amypo.in>
2025-12-18 09:23:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/RouteOptimizationResult.java
+++ src/main/java/com/example/demo/entity/RouteOptimizationResult.java
@@ -1,6 +1,11 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 public class RouteOptimizationResult {
     private Long id;
-    private Double optimi
+    private Double optimizedDistanceKm;
+    private Double estimatedFeulUsageL;
+    private LocalDateTime generatedAt;
+    
 }
