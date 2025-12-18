adde100b8edb4b74b42c0c818fb3db7d9c2e6566
bala-amypo <bala@amypo.in>
2025-12-18 13:37:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/serviceimpl/StudentServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.demo.serviceimpl;
+import org.springframework.stereotype.Service;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.entity.Student;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.service.StudentService;
+import java.util.List;
+@Service
+public class StudentServiceImpl implements StudentService {
+private final StudentRepository repo;
+public StudentServiceImpl(StudentRepository repo) {
+this.repo = repo;
+}
+@Override
+public Student saveStudent(Student student) {
+return repo.save(student);
+}
+@Override
+public Student getStudentById(Long id) {
+return repo.findById(id)
+.orElseThrow(() -> new
+ResourceNotFoundException("Student not found"));
+}
+@Override
+public List<Student> getAllStudents() {
+return repo.findAll();
+}
+@Override
+public Student updateStudent(Long id, Student student) {
+Student existing = getStudentById(id);
+existing.setName(student.getName());
+existing.setEmail(student.getEmail());
+return repo.save(existing);
+}
+@Override
+public void deleteStudent(Long id) {
+Student student = getStudentById(id);
+repo.delete(student);
+}
+}
