a990098374c7d4bb58209647dd3f1f9b8c223041
bala-amypo <bala@amypo.in>
2025-12-18 08:53:41 +0000

    User commit

--- src/main/java/com/example/demo/entity/Vehicle.java
+++ src/main/java/com/example/demo/entity/Vehicle.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
 public class Vehicle {
@@ -11,6 +12,7 @@ public class Vehicle {
     private String vehicleNumber;
     @Min(100)
     private Double capacityKg;
+    @Max(10000)
     private Double fuelEfficiency;
 
 
